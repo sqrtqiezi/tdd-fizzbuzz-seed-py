4e7415459945ecaf1ce16335b21beb14e205d9b5
njin <njin.cool@gmail.com>
2020-11-06 16:49:05 +0800

    init seed

---
+++ fizzbuzz/__init__.py
@@ -0,0 +1,6 @@
+""" Taxi """
+
+
+def your_function(file):
+    """TODO: rename and start coding"""
+    return file
---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,5 @@
+import fizzbuzz
+
+
+def test_fizzbuzz():
+    assert taxi.your_function('Hello world!') == 'Hello world!'
