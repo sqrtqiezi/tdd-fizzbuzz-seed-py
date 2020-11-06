09b4747c83d66436c1a7b28dafd56ae025ae9a5d
njin <njin.cool@gmail.com>
2020-11-06 20:04:47 +0800

    sucess sample

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,21 +6,18 @@ pipeline {
     }
 
     stages {
-        stage('Build') {
+        stage('Install Requirements') {
             steps {
-                echo 'Building..'
                 sh 'pip install -i https://pypi.doubanio.com/simple/ -r ./requirements.txt'
             }
         }
         stage('Style Check') {
             steps {
-                echo 'Checking'
                 sh 'python -m pylint fizzbuzz'
             }
         }
         stage('Test') {
             steps {
-                echo 'testing'
                 sh 'python -m pytest --cov-branch --cov=fizzbuzz tests/ --cov-report xml:coverage.xml'
             }
         }
--- fizzbuzz/__init__.py
+++ fizzbuzz/__init__.py
@@ -1,5 +1,27 @@
 """ Fizzbuzz """
 
-def your_function(file):
-    """TODO: rename and start coding"""
-    return file
+def process_file(path):
+    """process the test data"""
+    result = []
+    with open(path, "r") as file:
+        line = file.readline()
+        while line:
+            number = int(line)
+            result.append(fizzbuzz(number))
+            line = file.readline()
+    return "\n".join(map(lambda item: str(item), result))
+
+def __is_divide_by_or_contains(number, divisor):
+    return number % divisor == 0 \
+        or str(number).find(str(divisor)) != -1
+
+def fizzbuzz(number):
+    """fizzbuzz"""
+    result = ""
+    if __is_divide_by_or_contains(number, 3):
+        result += "Fizz"
+    if __is_divide_by_or_contains(number, 5):
+        result += "Buzz"
+    if result != "":
+        return result
+    return number
